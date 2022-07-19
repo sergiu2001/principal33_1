ded20f028838f075801540e92a2f90ace88ced5e
sergiu2001 <sergiu.frasie@ulbsibiu.ro>
2022-07-19 13:04:43 +0300

    Init

--- functions.js
+++ functions.js
@@ -1,3 +1,23 @@
-function alertPopUp() {
-    alert("Today is the 18th of July!");
+function alertPopUp(day) {
+    alert(`Today is the ${day}th of July!`);
+}
+
+function createTable(limit) {
+    var tab = document.getElementsByClassName("calendar__content");
+
+    var days = [];
+    var cells = [];
+
+    for (var i = 1; i < limit; i++) {
+        days[i] = i;
+    }
+
+    for (var j = 1; j < 5; j++) {
+        tab.insertRow(j - 1);
+        if (j < 4) {
+            for (var k = 1; k < 8; k++) {
+                cells[k + 7 * j]
+            }
+        }
+    }
 }
