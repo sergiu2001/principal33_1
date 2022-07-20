b44d0dde183cf5443f351e8799f304d0ee04338b
sergiu2001 <sergiu.frasie@ulbsibiu.ro>
2022-07-20 12:53:25 +0300

    Init

--- functions.js
+++ functions.js
@@ -1,5 +1,11 @@
 function alertPopUp(day) {
-    alert(`Today is the ${day}th of July!`);
+    if(day%10==1 && day!=11)
+        alert(`Today is the ${day}st of July!`);
+    else if(day%10==2 && day!=12)
+        alert(`Today is the ${day}nd of July!`);
+    else  if(day%10==3 && day!=13)
+        alert(`Today is the ${day}rd of July!`);
+    else  alert(`Today is the ${day}th of July!`);
 }
 
 function createTable(limit) {
